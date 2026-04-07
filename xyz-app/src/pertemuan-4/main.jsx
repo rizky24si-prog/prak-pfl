a569b4ab30a163ab15f5f0756935b8280cecf9dd
rizky24si <rizky24si@mahasiswa.pcr.ac.id>
2026-04-07 14:48:41 +0700

    Json

---
+++ xyz-app/src/pertemuan-4/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import './tailwind.css';
+import Frameworklist from "./Frameworklist";
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+            HAI INI PERTEMUAN 4 Kby
+            <Frameworklist/>
+        </div>
+    )
